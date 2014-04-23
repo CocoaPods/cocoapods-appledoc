34458239ca4e44aa6cf02ce34ecf988b6f882dfc
Kyle Fuller <inbox@kylefuller.co.uk>
2014-04-23 01:17:04 +0100

    Basic implementation to generate a docset

--- lib/pod/command/appledoc.rb
+++ lib/pod/command/appledoc.rb
@@ -1,3 +1,4 @@
+require 'shellwords'
 require 'cocoapods'
 
 module Pod
@@ -15,11 +16,68 @@ module Pod
         def validate!
           super
           help! "A spec name is required" unless @name
+          help! 'Please install appledoc first' if `which appledoc`.empty?
+        end
+
+        def download_location
+          File.join(APPLEDOC_DOWNLOAD_DIRECTORY, "#{@spec.name}-#{@spec.version}")
+        end
+
+        def download
+          downloader = Pod::Downloader.for_target(download_location, @spec.source)
+          downloader.download
+        end
+
+        def public_headers_files
+          headers = []
+
+          pathlist = Pod::Sandbox::PathList.new(Pathname.new(download_location))
+          [@spec, *@spec.recursive_subspecs].each do |internal_spec|
+            internal_spec.available_platforms.each do |platform|
+              consumer = Pod::Specification::Consumer.new(internal_spec, platform)
+              accessor = Pod::Sandbox::FileAccessor.new(pathlist, consumer)
+
+              if accessor.public_headers
+                headers += accessor.public_headers.map{ |filepath| filepath.to_s }
+              end
+            end
+          end
+
+          headers.uniq
+        end
+
+        def generate_docset
+          escaped_headers = public_headers_files.map do |header_file|
+            Shellwords.escape(header_file)
+          end
+
+          command = [
+            'appledoc',
+            "--company-id 'org.cocoadocs.#{@spec.name.downcase}'",
+            "--project-name '#{@spec.name}'",
+            "--project-company '#{@spec.authors.to_s}'",
+            "--project-version '#{@spec.version}'",
+            '--no-install-docset', # Don't install the docset into Xcode
+            '--create-html',
+            '--publish-docset',
+            "--output #{Shellwords.escape(output_location)}",
+            *escaped_headers
+          ]
+
+          system command.join(' ')
+        end
+
+        def output_location
+          File.join(Dir.pwd, "#{@spec.name}-#{@spec.version}")
         end
 
         def run
           @spec = spec_with_name(@name)
-          # TODO
+
+          raise Informative, "Docset in #{output_location} already exists" if File.exist?(output_location)
+
+          download unless File.exist?(download_location)
+          generate_docset
         end
 
         def spec_with_name(name)
@@ -31,6 +89,9 @@ module Pod
             raise Informative, "Unable to find a specification for `#{name}`"
           end
         end
+
+        APPLEDOC_TMP_DIR = Pathname.new('/tmp/CocoaPods/AppleDoc')
+        APPLEDOC_DOWNLOAD_DIRECTORY = File.join(APPLEDOC_TMP_DIR, 'Sources')
       end
     end
   end
